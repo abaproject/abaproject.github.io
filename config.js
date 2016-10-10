b5ea1d2c8bc4b65d7ec56707a17eaaa50820a424
Doeun Koch <koch.doeun@ABA.COM>
2016-10-10 10:09:45 +0700

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyCqStFL6hMU1Hb7VAP_Nh1cWL5dhOX5i7E'
+  gcmAPIKey: 'AIzaSyDq5WEWwiKCcDotArJXJUY6gX2AEDcxArM'
 };
